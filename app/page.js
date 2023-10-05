41988d132a94bc232bf918fa75f73b6c2aff1767
woosol <wsol8934@gmail.com>
2023-10-05 21:02:51 +0900

    수정2

--- app/page.js
+++ app/page.js
@@ -2,16 +2,15 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
-    let name ='sol woo'
-    let link ='http://google.com'
+    let name = 'sol woo'
+    let link = 'http://google.com'
     return (
         <div>
             <h1 className='title'>fresh</h1>
             <p className='sub_title'>by {name}</p>
             <div className='link'>
-                <a href = {link} target='_blank' title='새창' style={{color:'#fff', fontSize:'12px'}} >google_link</a>
+                <a href={link} target='_blank' title='새창' style={{ color: '#fff', fontSize: '12px' }} >google_link</a>
             </div>
-            <div>메인페이지만들기</div>
         </div>
     )
 }
